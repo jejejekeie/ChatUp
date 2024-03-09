8dcd1b61ac5ebebcee2fbdf4b7950ceb29c8f50e
jejejekeie <evap040@gmail.com>
2024-03-09 21:57:20 +0100

    Commit inicial backend

---
+++ backend/chatUp/src/main/java/com/example/chatup/ChatUpApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatUpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatUpApplication.class, args);
+    }
+
+}
